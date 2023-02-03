30589b2ec7882eca1b55c8b1e0b7a58f2caa0cc6
oswalderon <62608837+oswalderon@users.noreply.github.com>
2023-02-02 22:34:30 -0600

    Add files via upload

---
+++ templates/Footer.php
@@ -0,0 +1,5 @@
+        <footer class="footer fixed-bottom">
+                    <p class="text-center"> Todos los derechos reservados, FSJ16 2022</p>
+        </footer>
+    </body>
+</html>
---
+++ templates/Header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PATITAS SUAVES</title>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    <link href='https://fonts.googleapis.com/css?family=Caveat Brush' rel='stylesheet'>
+  </head>
+<body>
+  
+<div class="nav">
+  <nav class="navbar navbar-expand-lg m-3">
+  <p class="text">Veterinaria Patitas Suaves 🐾</p>
+  </nav>
+  <div class="container-fluid">
+    <ul class="nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.php">Inicio</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Empleados.php">Empleados</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="Pacientes.php">Pacientes</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled">Inventario</a>
+      </li>
+    </ul>  
+  </div>
+</div>
